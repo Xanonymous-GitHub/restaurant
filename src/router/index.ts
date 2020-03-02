472ee92bfd0c96e42c6d7ca2f4bbf1b1c0e3d27f
Xanonymous <trusaidlin@gmail.com>
2020-03-02 20:13:58 +0800

    use vue class components --add files.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+const BackGround = () => import("@/components/BackGround.vue");
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'BackGround',
+    component: BackGround
   },
   {
     path: '/about',
