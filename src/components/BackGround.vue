c1156d51df2bb26d5d4a74ccd49d8a3b6d0059cb
Xanonymous <trusaidlin@gmail.com>
2020-03-02 20:41:23 +0800

    make js-type scripts to ts-type.

--- src/components/BackGround.vue
+++ src/components/BackGround.vue
@@ -3,10 +3,10 @@
     <v-row class="text-center">
       <v-col cols="12">
         <v-img
-                :src="require('../assets/logo.svg')"
                 class="my-3"
                 contain
                 height="200"
+                src="../assets/logo.svg"
         />
       </v-col>
 
@@ -97,7 +97,57 @@
 
   @Component
   export default class BackGround extends Vue {
-    next = "";
+    ecosystem = [
+      {
+        text: 'vuetify-loader',
+        href: 'https://github.com/vuetifyjs/vuetify-loader',
+      },
+      {
+        text: 'github',
+        href: 'https://github.com/vuetifyjs/vuetify',
+      },
+      {
+        text: 'awesome-vuetify',
+        href: 'https://github.com/vuetifyjs/awesome-vuetify',
+      },
+    ];
+    importantLinks = [
+      {
+        text: 'Documentation',
+        href: 'https://vuetifyjs.com',
+      },
+      {
+        text: 'Chat',
+        href: 'https://community.vuetifyjs.com',
+      },
+      {
+        text: 'Made with Vuetify',
+        href: 'https://madewithvuejs.com/vuetify',
+      },
+      {
+        text: 'Twitter',
+        href: 'https://twitter.com/vuetifyjs',
+      },
+      {
+        text: 'Articles',
+        href: 'https://medium.com/vuetify',
+      },
+    ];
+    whatsNext = [
+      {
+        text: 'Explore components',
+        href: 'https://vuetifyjs.com/components/api-explorer',
+      },
+      {
+        text: 'Select a layout',
+        href: 'https://vuetifyjs.com/layout/pre-defined',
+      },
+      {
+        text: 'Frequently Asked Questions',
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+      },
+    ];
+
   }
 
 </script>
